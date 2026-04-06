930810e77582a156f7e72d595639e884e12bdeff
Deepanshu Singh <144131132+deepanshusinghrajpoot@users.noreply.github.com>
2026-04-06 12:10:22 +0530

    Update quality_checks_silver.sql

--- test/silver/quality_checks_silver.sql
+++ test/silver/quality_checks_silver.sql
@@ -25,7 +25,7 @@ Usage Notes:
 SELECT 
     cst_id,
     COUNT(*) 
-FROM silver.crm_cust_info
+FROM datawarehouse.silver_crm_cust_info
 GROUP BY cst_id
 HAVING COUNT(*) > 1 OR cst_id IS NULL;
 
@@ -33,13 +33,13 @@ HAVING COUNT(*) > 1 OR cst_id IS NULL;
 -- Expectation: No Results
 SELECT 
     cst_key 
-FROM silver.crm_cust_info
+FROM datawarehouse.silver_crm_cust_info
 WHERE cst_key != TRIM(cst_key);
 
 -- Data Standardization & Consistency
 SELECT DISTINCT 
     cst_marital_status 
-FROM silver.crm_cust_info;
+FROM datawarehouse.silver_crm_cust_info;
 
 -- ====================================================================
 -- Checking 'silver.crm_prd_info'
@@ -49,7 +49,7 @@ FROM silver.crm_cust_info;
 SELECT 
     prd_id,
     COUNT(*) 
-FROM silver.crm_prd_info
+FROM datawarehouse.silver_crm_prd_info
 GROUP BY prd_id
 HAVING COUNT(*) > 1 OR prd_id IS NULL;
 
@@ -57,26 +57,26 @@ HAVING COUNT(*) > 1 OR prd_id IS NULL;
 -- Expectation: No Results
 SELECT 
     prd_nm 
-FROM silver.crm_prd_info
+FROM datawarehouse.silver_crm_prd_info
 WHERE prd_nm != TRIM(prd_nm);
 
 -- Check for NULLs or Negative Values in Cost
 -- Expectation: No Results
 SELECT 
     prd_cost 
-FROM silver.crm_prd_info
+FROM datawarehouse.silver_crm_prd_info
 WHERE prd_cost < 0 OR prd_cost IS NULL;
 
 -- Data Standardization & Consistency
 SELECT DISTINCT 
     prd_line 
-FROM silver.crm_prd_info;
+FROM datawarehouse.silver_crm_prd_info;
 
 -- Check for Invalid Date Orders (Start Date > End Date)
 -- Expectation: No Results
 SELECT 
     * 
-FROM silver.crm_prd_info
+FROM datawarehouse.silver_crm_prd_info
 WHERE prd_end_dt < prd_start_dt;
 
 -- ====================================================================
@@ -86,9 +86,9 @@ WHERE prd_end_dt < prd_start_dt;
 -- Expectation: No Invalid Dates
 SELECT 
     NULLIF(sls_due_dt, 0) AS sls_due_dt 
-FROM bronze.crm_sales_details
+FROM datawarehouse.bronze_crm_sales_details
 WHERE sls_due_dt <= 0 
-    OR LEN(sls_due_dt) != 8 
+    OR LENGTH(sls_due_dt) != 8 
     OR sls_due_dt > 20500101 
     OR sls_due_dt < 19000101;
 
@@ -96,7 +96,7 @@ WHERE sls_due_dt <= 0
 -- Expectation: No Results
 SELECT 
     * 
-FROM silver.crm_sales_details
+FROM datawarehouse.silver_crm_sales_details
 WHERE sls_order_dt > sls_ship_dt 
    OR sls_order_dt > sls_due_dt;
 
@@ -106,7 +106,7 @@ SELECT DISTINCT
     sls_sales,
     sls_quantity,
     sls_price 
-FROM silver.crm_sales_details
+FROM datawarehouse.silver_crm_sales_details
 WHERE sls_sales != sls_quantity * sls_price
    OR sls_sales IS NULL 
    OR sls_quantity IS NULL 
@@ -120,17 +120,17 @@ ORDER BY sls_sales, sls_quantity, sls_price;
 -- Checking 'silver.erp_cust_az12'
 -- ====================================================================
 -- Identify Out-of-Range Dates
--- Expectation: Birthdates between 1924-01-01 and Today
+-- Expectation: Birthdates between 1915-01-01 and Today
 SELECT DISTINCT 
     bdate 
-FROM silver.erp_cust_az12
-WHERE bdate < '1924-01-01' 
-   OR bdate > GETDATE();
+FROM datawarehouse.silver_erp_cust_az12
+WHERE bdate < '1915-01-01' 
+   OR bdate > CURRENT_DATE;
 
 -- Data Standardization & Consistency
 SELECT DISTINCT 
     gen 
-FROM silver.erp_cust_az12;
+FROM datawarehouse.silver_erp_cust_az12;
 
 -- ====================================================================
 -- Checking 'silver.erp_loc_a101'
@@ -138,7 +138,7 @@ FROM silver.erp_cust_az12;
 -- Data Standardization & Consistency
 SELECT DISTINCT 
     cntry 
-FROM silver.erp_loc_a101
+FROM datawarehouse.silver_erp_loc_a101
 ORDER BY cntry;
 
 -- ====================================================================
@@ -148,7 +148,7 @@ ORDER BY cntry;
 -- Expectation: No Results
 SELECT 
     * 
-FROM silver.erp_px_cat_g1v2
+FROM datawarehouse.silver_erp_px_cat_g1v2
 WHERE cat != TRIM(cat) 
    OR subcat != TRIM(subcat) 
    OR maintenance != TRIM(maintenance);
@@ -156,4 +156,4 @@ WHERE cat != TRIM(cat)
 -- Data Standardization & Consistency
 SELECT DISTINCT 
     maintenance 
-FROM silver.erp_px_cat_g1v2;
+FROM datawarehouse.silver_erp_px_cat_g1v2;
